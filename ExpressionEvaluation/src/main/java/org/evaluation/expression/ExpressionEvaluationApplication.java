4216454c32372c6027b73f999a206e1b3c1eb12b
Pawan Maurya <31837242+MisterMaurya@users.noreply.github.com>
2018-11-27 13:33:38 +0530

    Initial Commit

---
+++ ExpressionEvaluation/src/main/java/org/evaluation/expression/ExpressionEvaluationApplication.java
@@ -0,0 +1,12 @@
+package org.evaluation.expression;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpressionEvaluationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpressionEvaluationApplication.class, args);
+	}
+}
